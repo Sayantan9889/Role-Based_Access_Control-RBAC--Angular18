0aa14a5fcecfe5acd2b052d191277f0e1a58f7a0
Sayantan Singha <sayantan.singha@webskitters.in>
2024-11-18 20:31:52 +0530

    side bar ongoing

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../shared/header/header.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
