102f4d0858390e36f75f82333904206de05f7aa6
Sayantan Singha <sayantan.singha@webskitters.in>
2024-11-22 12:32:06 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Assessment-11-11-2024';
   private document = inject(DOCUMENT);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    // this.router.initialNavigation();
+  }
 
   protected sidenavOpenClose(event: 'open' | 'close') {
     const navContainer: Element | null = this.document.querySelector('.body-container');
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withDisabledInitialNavigation, withEnabledBlockingInitialNavigation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -11,7 +11,14 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withDisabledInitialNavigation(),
+      withViewTransitions(),
+      withEnabledBlockingInitialNavigation(),
+      withComponentInputBinding()
+    ),
     provideClientHydration(),
     provideHttpClient(withFetch(), withInterceptorsFromDi(), withInterceptors([httpInterceptor])),
     provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()
